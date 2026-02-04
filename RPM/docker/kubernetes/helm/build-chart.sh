7deb5cd21000b189e184bbfea7408f430e87088a
Ron Petrov <rpetrov@nvidia.com>
2026-02-04 14:47:18 +0200

    Remove RPM/docker dir (resolves NVMESH-7600)
    * As part of open sourcing we should remove RPM/docker as it doesn't seem necessary anymore
    
    Change-Id: I2944dc0568ac7107e24ed6624a3cbea4a2b52d1d
    Signed-off-by: Ron <rpetrov@nvidia.com>

--- RPM/docker/kubernetes/helm/build-chart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-helm package nvmesh-management --destination .
