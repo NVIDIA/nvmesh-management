f4bdd80bc40c37a9dd38820f69ece11d4c64ea90
Yaniv Romem <yaniv@nvidia.com>
2026-03-17 11:36:08 +0200

    Add SPDX to the newer files.
    Change-Id: I98f647f133fb4c2e138de105c5f1fc0b731fa909
    Signed-off-by: Yaniv <yaniv@nvidia.com>

--- scripts/certificateGeneration/generateCertificatesFromCSV.py
+++ scripts/certificateGeneration/generateCertificatesFromCSV.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 import argparse
 import os
 import csv
